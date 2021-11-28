6898d179ec49ddb520db99fecfd4c14f664014c5
Brian <73376598+batuh836@users.noreply.github.com>
2021-11-28 11:31:13 -0700

    React

--- play-server.js
+++ play-server.js
@@ -57,7 +57,7 @@ userRouter.handleSingleUser(app, User);
 app.use('/', express.static(path.join(__dirname, 'public', 'build')));
 
 app.get('/', helper.ensureAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 //app.get('/user', helper.ensureAuthenticated, (req, res) => {
