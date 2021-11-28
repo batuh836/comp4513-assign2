a85f5437b9bf4c96c2d56ee51f846000801ae98f
Brian <73376598+batuh836@users.noreply.github.com>
2021-11-28 15:07:54 -0700

    m

--- public/src/components/User.js
+++ public/src/components/User.js
@@ -8,14 +8,14 @@ const User = (props) => {
     }
     
     if (props.user) {
-        const profile = props.user.profile;
+        const details = props.user.details;
         const membership = props.user.membership;
         const picture = props.user.picture;
         return (
             <Modal isOpen={props.modalIsOpen} contentLabel="Example Modal" className="user">
                 <h1>Profile</h1>
-                <span>Name: {profile.firstname} {profile.lastname}</span>
-                <span>Location: {profile.city}, {profile.country}</span>
+                <span>Name: {details.firstname} {details.lastname}</span>
+                <span>Location: {details.city}, {details.country}</span>
                 <span>Joined: {membership.date_joined}</span>
                 <img src={picture.thumbnail} alt="picture"/>
                 <button onClick={handleOnClick}>Close</button>
