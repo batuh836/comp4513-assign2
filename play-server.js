6081d5ce3652a93eacb92078346b0edf7cb7400e
Brian <73376598+batuh836@users.noreply.github.com>
2021-11-28 13:09:49 -0700

    Update play-server.js

--- play-server.js
+++ play-server.js
@@ -87,7 +87,7 @@ app.use(function (req, res, next) {
     res.status(404).send("Sorry can't find that!")
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.listen(port, () => {
     console.log("Server running at port= " + port);
 });
