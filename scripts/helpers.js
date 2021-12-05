28c93142c4c4bacc6a4781ecce16ee7a52c93ebb
Brian <73376598+batuh836@users.noreply.github.com>
2021-12-05 13:30:31 -0700

    help

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,12 +1,12 @@
 // uses passport authentication to check if authentication is
 // needed at some point in middleware pipeline.
 function ensureAuthenticated (req, resp, next) {
-    console.log(req)
+    console.log("ensureAuthenticated");
 //    if (req.isAuthenticated()) {
 //        return next();
 //    }
     
-    req.flash('info', 'Please log in to view that resource');
-    resp.render('login.ejs', {message: req.flash('info')} );
+//    req.flash('info', 'Please log in to view that resource');
+//    resp.render('login.ejs', {message: req.flash('info')} );
 }
 module.exports = { ensureAuthenticated };
