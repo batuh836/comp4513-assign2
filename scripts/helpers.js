a007b9fa8025326e70e654e43e3f0f3e30105897
Brian <73376598+batuh836@users.noreply.github.com>
2021-12-10 13:08:09 -0700

    Update helpers.js

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -3,12 +3,13 @@
 function ensureAuthenticated (req, res, next) {
     if (req.isAuthenticated()) {
         console.log("is authenticated");
-//        return next();
+        return next();
     }
     else {
         console.log("is NOT authenticated");
         req.flash('info', 'Please log in to view that resource');
         res.render('login', {message: req.flash('info')} );
+        return null;
     }
 }
 module.exports = { ensureAuthenticated };
